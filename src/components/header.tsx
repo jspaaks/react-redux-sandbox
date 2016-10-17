3cb3ef18f34ff80c305114ed77c588461ca0b94c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-10-17 15:39:11 +0200

    Header component now knows about what props to expect; it has an input box, that can fire onchange events

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-export default class Header extends React.Component<{}, {}> {
+import { PageLayout } from "./page-layout";
+
+
+type props = {
+    title:string,
+    changeTitle: (title:string) => PageLayout
+};
+
+export default class Header extends React.Component<props, {}> {
 
     constructor () {
         super();
     }
 
+    public onChange(e: any) {
+        // e is the event you get from the 'input' element
+        let str: string = "A whole new title " + Date.now().toString() + e.target.value;
+        this.props.changeTitle(str);
+    }
+
+
     public render() {
         let tsx = (
-            <p className="header">WordStyler</p>
+            <div>
+                <p className="header">{this.props.title}</p>
+                <input defaultValue="type here" onChange={this.onChange.bind(this)}/>
+            </div>
         );
         return tsx;
     }
