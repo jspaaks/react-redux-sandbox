0d86ba39a3da12fe2a26f3677eedd251c9064229
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-10-17 18:07:00 +0200

    added getters and setters for the state; also added serialization and deserialization

--- src/components/item.ts
+++ src/components/item.ts
@@ -8,27 +8,25 @@ export default class Item {
     private static readonly underlined = 4;
 
     public id: number;
-    private isbold: boolean;
-    private isitalic: boolean;
-    private isunderlined: boolean;
-    private serialized: number;
+    private _isbold: boolean;
+    private _isitalic: boolean;
+    private _isunderlined: boolean;
+    private _serialized: number;
     public readonly word: string;
 
-
-    constructor(word: string, isitalic: boolean = false, isbold: boolean = false, isunderlined: boolean = false) {
+    constructor(word: string, isbold: boolean = false, isitalic: boolean = false, isunderlined: boolean = false) {
         this.word = word;
-        this.isitalic = isitalic;
         this.isbold = isbold;
+        this.isitalic = isitalic;
         this.isunderlined = isunderlined;
         this.id = Item.id;
         Item.id += 1;
-        this.serialize();
     }
 
 
     public clone(retainId:boolean = true): Item {
 
-        let clone:Item = new Item(this.word, this.isitalic, this.isbold, this.isunderlined);
+        let clone:Item = new Item(this.word, this.isbold, this.isitalic, this.isunderlined);
         if (retainId) {
             // use the old id in the new object
             clone.id = this.id;
@@ -40,17 +38,43 @@ export default class Item {
 
 
     private serialize(): Item {
+        // make _serialized consistent with the values of _isbold, _isitalic,
+        // and _isunderlined
         let serialized = 0;
-        if (this.isbold) {
+        if (this._isbold) {
             serialized += Item.bold;
         }
-        if (this.isitalic) {
+        if (this._isitalic) {
             serialized += Item.italic;
         }
-        if (this.isunderlined) {
+        if (this._isunderlined) {
             serialized += Item.underlined;
         }
-        this.serialized = serialized;
+        this._serialized = serialized;
+        return this;
+    }
+
+
+    private deserialize(): Item {
+        // make the values of _isbold, _isitalic, and _isunderlined consistent
+        // with the value of _serialized
+
+        let serialized: number = this._serialized;
+        if (serialized > Item.underlined + Item.italic + Item.bold) {
+            throw new Error("number too big to deserialize");
+        }
+        if (serialized - Item.underlined > 0) {
+            this._isunderlined = true;
+            serialized -= Item.underlined;
+        }
+        if (serialized - Item.italic > 0) {
+            this._isitalic = true;
+            serialized -= Item.italic;
+        }
+        if (serialized - Item.bold > 0) {
+            this._isbold = true;
+            serialized -= Item.bold;
+        }
         return this;
     }
 
@@ -59,23 +83,52 @@ export default class Item {
         switch (str) {
             case "bold": {
                 this.isbold = !this.isbold;
-                this.serialize();
                 return this;
             }
             case "italic": {
                 this.isitalic = !this.isitalic;
-                this.serialize();
                 return this;
             }
             case "underlined": {
                 this.isunderlined = !this.isunderlined;
-                this.serialize();
                 return this;
             }
             default: {
                 throw new Error("Can't toggle unknown property.");
             }
         }
+    }
+
+    public get serialized(): number {
+        return this._serialized;
+    }
+    public set serialized(serialized: number) {
+        this._serialized = serialized;
+        this.deserialize();
+    }
+
+    public get isbold(): boolean {
+        return this._isbold;
+    }
+    public set isbold(isbold:boolean) {
+        this._isbold = isbold;
+        this.serialize();
+    }
+
+    public get isitalic(): boolean {
+        return this._isitalic;
+    }
+    public set isitalic(isitalic:boolean) {
+        this._isitalic = isitalic;
+        this.serialize();
+    }
+
+    public get isunderlined(): boolean {
+        return this._isunderlined;
+    }
+    public set isunderlined(isunderlined:boolean) {
+        this._isunderlined = isunderlined;
+        this.serialize();
     }
 
 }
