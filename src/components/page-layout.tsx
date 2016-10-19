d3a8d76873c8ae94772bbaaf14b2aec25f018053
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-10-19 16:45:28 +0200

    cleaned up PageLayout; removed some on* methods as part of the transition to redux; removed initializing state in the constructor

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -6,14 +6,13 @@ import TableItemAdder from "./table-item-adder";
 import Footer         from "./footer";
 import Item           from "./item";
 
-type props = {};
-type state = {
+type props = {
     title: string,
     items: Item[]
 };
+type state = {
+};
 
-
-let firstrun = true;
 
 export class PageLayout extends React.Component<props, state> {
 
@@ -21,67 +20,18 @@ export class PageLayout extends React.Component<props, state> {
     constructor() {
         // call super to inherit from React.Component
         super();
-
-        this.state = {
-            title: "WordStylerz",
-            items: [
-                new Item("no style", false, false, false),
-                new Item("onlybold", true, false, false),
-                new Item("only italic", false, true, false),
-                new Item("bold+italic", true, true, false),
-                new Item("only underlined", false, false, true),
-                new Item("bold + underlined", true, false, true),
-                new Item("italic + underlined", false, true, true),
-                new Item("bold+italic+underlined", true, true, true)
-            ]
-        };
     }
 
-    public changeTitle(title: string): PageLayout {
-        // not react / flux compliant:
-        this.state.title = title;
-        this.setState(this.state);
-        return this;
-    }
-
-
-    public addItem(item:Item): PageLayout {
-        this.state.items.push(item);
-        this.setState(this.state);
-        return this;
-    }
-
-
-    public removeItem(id:number): PageLayout {
-
-        this.state.items = this.state.items.filter((item:Item) => {
-            return item.id !== id;
-        });
-        this.setState(this.state);
-        return this;
-    }
 
 
 
     public render () {
 
-        if (firstrun) {
-            setTimeout(() => {
-                let item: Item = new Item("added item");
-                this.addItem(item);
-            }, 3000);
-            setTimeout(() => {
-                let id: number = 0;
-                this.removeItem(id);
-            }, 6000);
-            firstrun = false;
-        }
-
         let tsx = (
             <div>
-                <Header title={this.state.title} changeTitle={this.changeTitle.bind(this)}/>
-                <Table items={this.state.items} />
-                <TableItemAdder addItem={this.addItem.bind(this)}/>
+                <Header title={this.props.title} />
+                <Table items={this.props.items} />
+                <TableItemAdder />
                 <Footer />
             </div>
         );
