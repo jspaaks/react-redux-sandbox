812cb849c7f12d8b8ebce16f864ba241e87ebbd4
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-10-19 16:46:12 +0200

    removed onClick method as part of the transition to redux

--- src/components/table-item-adder.tsx
+++ src/components/table-item-adder.tsx
@@ -4,7 +4,6 @@ import Item          from "./item";
 
 
 type props = {
-    addItem: (item:Item) => {},
 };
 type state = {
 };
@@ -18,20 +17,11 @@ export default class TableItemAdder extends React.Component<props, state> {
     }
 
 
-    public onClick(e:any) {
-
-        // let word = e.target.value;
-        console.error("still have to get text from input box ");
-        let item: Item = new Item("word");
-        this.props.addItem(item);
-    }
-
-
     public render() {
         let tsx = (
             <form>
                 <input type="text" defaultValue="Add word..."/>
-                <input type="submit" onClick={this.onClick.bind(this)}/>
+                <input type="submit" />
             </form>
         );
         return tsx;
